c5a94a58d9dca18d0ddd1a48de88188bd04c283d
akhilps1 <akhilps595@gmail.com>
2025-06-25 00:41:42 +0530

    .

--- contracts/Permissions.sol
+++ contracts/Permissions.sol
@@ -1,23 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract Permissions is Initializable, OwnableUpgradeable {
-    enum Access { Create, Update, Remove, Read }
-    mapping(address => mapping(Access => bool)) public userPermissions;
+contract Permissions is Initializable, AccessControlUpgradeable {
+    bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
+    bytes32 public constant TREASURER_ROLE = keccak256("TREASURER_ROLE");
 
-    function initialize() public initializer {
-        __Ownable_init(msg.sender);
+    function initialize(address admin) public initializer {
+        __AccessControl_init();
+       grantRole(DEFAULT_ADMIN_ROLE, admin);
+       grantRole(OPERATOR_ROLE, admin);
     }
 
-    function setAccess(address user, Access accessLevel) external onlyOwner {
-        userPermissions[user][accessLevel] = true;
+    function grantOperator(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        grantRole(OPERATOR_ROLE, account);
     }
 
-    function hasAccess(address user, Access accessLevel) external view returns (bool) {
-        return userPermissions[user][accessLevel];
+    function revokeOperator(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        revokeRole(OPERATOR_ROLE, account);
+    }
+
+    function grantTreasurer(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        grantRole(TREASURER_ROLE, account);
+    }
+
+    function revokeTreasurer(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        revokeRole(TREASURER_ROLE, account);
+    }
+
+    function hasOperator(address account) external view returns (bool) {
+        return hasRole(OPERATOR_ROLE, account);
+    }
+
+    function hasTreasurer(address account) external view returns (bool) {
+        return hasRole(TREASURER_ROLE, account);
     }
 }
--- contracts/UCPI.sol
+++ contracts/UCPI.sol
@@ -5,16 +5,27 @@ pragma solidity ^0.8.2;
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import "./Permissions.sol";
 import "./RewardManager.sol";
 import "./Logger.sol";
 
 
-contract UCPI is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+contract UCPI is Initializable, UUPSUpgradeable, OwnableUpgradeable, AccessControlUpgradeable {
     Permissions public permissions;
     RewardManager public rewardManager;
     Logger public logger;
+    address public treasury;
+
+    mapping(address => uint256) public lastActionTimestamp;
+    uint256 public minInterval;
+    uint256 public maxGasPrice;
+
+    event TreasuryChanged(address indexed oldTreasury, address indexed newTreasury);
+    event MinIntervalChanged(uint256 oldInterval, uint256 newInterval);
+    event MaxGasPriceChanged(uint256 oldPrice, uint256 newPrice);
 
     function initialize(address _permissions, address _rewardManager, address _logger) public initializer {
         __Ownable_init(msg.sender);
@@ -23,17 +34,66 @@ contract UCPI is Initializable, UUPSUpgradeable, OwnableUpgradeable {
         permissions = Permissions(_permissions);
         rewardManager = RewardManager(_rewardManager);
         logger = Logger(_logger);
-        
-        
     }
 
-    function performAction(address user,  uint256 amount, uint48 timestamp, string calldata txHash, address token) external {
-        require(permissions.hasAccess(msg.sender, Permissions.Access.Create), "No access");
+    function setTreasury(address _treasury) external onlyRole(permissions.TREASURER_ROLE()) {
+        address oldTreasury = treasury;
+        treasury = _treasury;
+        emit TreasuryChanged(oldTreasury, _treasury);
+    }
 
-        uint256 reward = rewardManager.calculateReward(amount, user);
-        logger.logTransaction(timestamp, user, amount, amount * rewardManager.platformFee() / 10000, reward, txHash, token);
-        rewardManager.distributeReward(user, amount, reward);
-        
+    function setMinInterval(uint256 _interval) external onlyRole(permissions.DEFAULT_ADMIN_ROLE()) {
+        uint256 oldInterval = minInterval;
+        minInterval = _interval;
+        emit MinIntervalChanged(oldInterval, _interval);
+    }
+
+    function setMaxGasPrice(uint256 _maxGasPrice) external onlyRole(permissions.DEFAULT_ADMIN_ROLE()) {
+        uint256 oldPrice = maxGasPrice;
+        maxGasPrice = _maxGasPrice;
+        emit MaxGasPriceChanged(oldPrice, _maxGasPrice);
+    }
+
+    modifier gasPriceCap() {
+        require(tx.gasprice <= maxGasPrice, "Gas price exceeds cap");
+        _;
+    }
+
+    function performTransaction(address recipient, uint256 amount, uint48 timestamp, string calldata txHash, address token) external payable gasPriceCap onlyRole(permissions.OPERATOR_ROLE()) {
+        require(recipient != address(0), "Invalid recipient");
+        require(block.timestamp - lastActionTimestamp[msg.sender] >= minInterval, "Rate limit: wait before next action");
+        lastActionTimestamp[msg.sender] = block.timestamp;
+
+        uint256 fee = amount * rewardManager.platformFee() / 10000;
+        uint256 netAmount = amount - fee;
+        uint256 reward = rewardManager.calculateReward(amount, recipient);
+
+        if (token == address(0)) {
+            // ETH payment
+            require(msg.value == amount, "Incorrect ETH sent");
+            if (fee > 0 && treasury != address(0)) {
+                payable(treasury).transfer(fee);
+            }
+            payable(recipient).transfer(netAmount);
+        } else {
+            // ERC20 payment
+            require(IERC20(token).transferFrom(msg.sender, address(this), amount), "ERC20 transfer failed");
+            if (fee > 0 && treasury != address(0)) {
+                require(IERC20(token).transfer(treasury, fee), "Fee transfer failed");
+            }
+            require(IERC20(token).transfer(recipient, netAmount), "Recipient transfer failed");
+        }
+
+        logger.logTransaction(
+            timestamp,
+            recipient,
+            amount,
+            fee,
+            reward,
+            txHash,
+            token
+        );
+        rewardManager.distributeReward(recipient, amount, reward);
     }
 
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
